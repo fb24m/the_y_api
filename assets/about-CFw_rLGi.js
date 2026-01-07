b512c5b5f6446870a0bc185ba7683eaa78bee4fc
fb24m <hello@fb24m.ru>
2026-01-07 14:06:09 +0400

    Add files via upload

---
+++ assets/about-CFw_rLGi.js
@@ -0,0 +1,1 @@
+import{c as t,j as n}from"./index-D9td5H3K.js";function c(){const o=t.c(1);let e;return o[0]===Symbol.for("react.memo_cache_sentinel")?(e=n.jsx("div",{children:'Hello "/about"!'}),o[0]=e):e=o[0],e}export{c as component};
